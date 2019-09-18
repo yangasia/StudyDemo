b050d77411d5a70f06d975e170b7b9d0b313cfd0
ASIA <yangyazhouweb@foxmail.com>
2019-09-18 23:46:33 +0800

    加载数据

--- webpack/webpack-demo/webpack.config.js
+++ webpack/webpack-demo/webpack.config.js
@@ -19,6 +19,24 @@ module.exports = {
                 use:[
                     'file-loader'
                 ]
+            },
+            {
+                test:/\.(woff|woff2|eot|ttf|otf)$/,
+                use:[
+                    'file-loader'
+                ]
+            },
+            {
+                test:/\.(csv|tsv)$/,
+                use:[
+                    'csv-loader'
+                ]
+            },
+            {
+                test:/\.xml$/,
+                use:[
+                    'xml-loader'
+                ] 
             }
         ]
     }
