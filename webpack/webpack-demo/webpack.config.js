4961d97adafe2ac34342404d6b2ad50cdbc70042
yangyazhou <yangyazhou.cq@qq.com>
2019-09-20 17:42:49 +0800

    动态生成index.html

--- webpack/webpack-demo/webpack.config.js
+++ webpack/webpack-demo/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 module.exports = {
     // entry:'./src/index.js',
     entry:{
@@ -7,6 +9,7 @@ module.exports = {
         print:'./src/print.js',
     },
     plugins:[
+        new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             title: 'Output Management'
         })
