1ec6791c01dd4de8c402d676f41b084a42381fc1
yangyazhou <yangyazhou.cq@qq.com>
2019-09-20 16:59:38 +0800

    准备管理输出

--- webpack/webpack-demo/src/index.js
+++ webpack/webpack-demo/src/index.js
@@ -1,20 +1,20 @@
 import _ from 'lodash';
-import './style.css';
-import y128 from './y128.png';
-import Data from './data.xml';
+// import './style.css';
+// import y128 from './y128.png';
+// import Data from './data.xml';
 
 function component(){
     var element = document.createElement('div');
     // lodash由js引入
 
     element.innerHTML = _.join(['Hello','webpack'],' ');
-    element.classList.add('hello');
+    // element.classList.add('hello');
     //加图片
-    var myICon = new Image();
-    myICon.src = y128;
+    // var myICon = new Image();
+    // myICon.src = y128;
 
-    element.appendChild(myICon);
-    console.log(Data);
+    // element.appendChild(myICon);
+    // console.log(Data);
     return element;
 }
 
