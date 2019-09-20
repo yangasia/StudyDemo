caec7eea276c682160593a6b793b7df0e33be67a
yangyazhou <yangyazhou.cq@qq.com>
2019-09-20 18:02:52 +0800

    清理dist目录完成

--- webpack/webpack-demo/webpack.config.js
+++ webpack/webpack-demo/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     // entry:'./src/index.js',
@@ -8,12 +8,12 @@ module.exports = {
         app:'./src/index.js',
         print:'./src/print.js',
     },
-    plugins:[
-        new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({
-            title: 'Output Management'
-        })
-    ],
+plugins:[
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+        title: 'Output Management'
+    })
+],
     output:{
         // filename:'bundle.js',
         filename:'[name].bundle.js',
