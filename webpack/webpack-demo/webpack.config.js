efdcc717ef882a03b19385d7159ab672b9fbfb76
yangyazhou <yangyazhou.cq@qq.com>
2019-09-20 17:39:20 +0800

    动态生成index.html

--- webpack/webpack-demo/webpack.config.js
+++ webpack/webpack-demo/webpack.config.js
@@ -1,16 +1,16 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin';)
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     // entry:'./src/index.js',
     entry:{
         app:'./src/index.js',
         print:'./src/print.js',
     },
-plugins:[
-    new HtmlWebpackPlugin({
-        title: 'Output Management'
-    })
-],
+    plugins:[
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ],
     output:{
         // filename:'bundle.js',
         filename:'[name].bundle.js',
