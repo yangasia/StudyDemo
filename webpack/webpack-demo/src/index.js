ba1a27e2ead1640ff2cc22b07e2df596a709dbfc
yangyazhou <yangyazhou.cq@qq.com>
2019-09-23 19:26:15 +0800

    模块懒加载

--- webpack/webpack-demo/src/index.js
+++ webpack/webpack-demo/src/index.js
@@ -1,4 +1,4 @@
-// import _ from 'lodash';
+import _ from 'lodash';
 // import { square,cube } from './math.js';
 // import printMe from './print.js';
 // import { from } from '_array-flatten@2.1.2@array-flatten';
@@ -13,48 +13,52 @@ if(process.env.NODE_EVN !== 'production'){
     console.log('-----------------------');
 }
 
-// function component(){
-//     var element = document.createElement('div');
-//     var btn = document.createElement('button');
-//     // lodash由js引入
+function component(){
+    var element = document.createElement('div');
+    var btn = document.createElement('button');
+    // lodash由js引入
 
-//     // element.innerHTML = _.join(['Hello','webpack'],' ');
-//     // element.innerHTML = [
-//     //     'hello webpack',
-//     //     '5的立方等于'+cube(5),
-//     //     '3的平方等于'+square(3),
-//     // ].join('\n\n');
+    element.innerHTML = _.join(['Hello','webpack'],' ');
+    // element.innerHTML = [
+    //     'hello webpack',
+    //     '5的立方等于'+cube(5),
+    //     '3的平方等于'+square(3),
+    // ].join('\n\n');
 
-//     btn.innerHTML = '点击我看console';
-//     btn.onclick = printMe;
-//     element.appendChild(btn);
-//     // element.classList.add('hello');
-//     //加图片
-//     // var myICon = new Image();
-//     // myICon.src = y128;
+    btn.innerHTML = '点击我看console';
+    // btn.onclick = printMe;
+    btn.onclick = e => import('./print').then(module => {
+        var print = module.default;
+        print();
+    });
+    element.appendChild(btn);
+    // element.classList.add('hello');
+    //加图片
+    // var myICon = new Image();
+    // myICon.src = y128;
 
-//     // element.appendChild(myICon);
-//     // console.log(Data);
-//     return element;
-// }
-
-// document.body.appendChild(component());
-
- async function getComponent() {
-    // return import(/* webpackChunkName: "lodash"*/ 'lodash').then(({default:_}) => {
-    //     const element = document.createElement('div');
-    //     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    //     return element;
-    // }).catch(error => '模块加载出错');
-    const element = document.createElement('div');
-    const { default:_ } = await import(/* webpackChunkName: "lodash"*/ 'lodash');
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    // element.appendChild(myICon);
+    // console.log(Data);
     return element;
 }
 
-getComponent().then(component => {
-    document.body.appendChild(component);
-})
+document.body.appendChild(component());
+
+//  async function getComponent() {
+//     // return import(/* webpackChunkName: "lodash"*/ 'lodash').then(({default:_}) => {
+//     //     const element = document.createElement('div');
+//     //     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//     //     return element;
+//     // }).catch(error => '模块加载出错');
+//     const element = document.createElement('div');
+//     const { default:_ } = await import(/* webpackChunkName: "lodash"*/ 'lodash');
+//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//     return element;
+// }
+
+// getComponent().then(component => {
+//     document.body.appendChild(component);
+// })
 
 // if (module.hot) {
 //     module.hot.accept('./print.js', function(){
