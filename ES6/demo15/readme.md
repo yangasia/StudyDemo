697304eb2000c666cfa054546ca9d8bdb04c2501
yangyazhou <yangyazhou.cq@qq.com>
2019-11-13 18:06:20 +0800

    Generator函数

--- ES6/demo15/readme.md
+++ ES6/demo15/readme.md
@@ -31,5 +31,153 @@ console.log(hw.next());
 //每一次next()调用都会被记住。再次调用从上次next的地方开始
 //
 //value表示状态值就是yield或者return后面的值，如果没有值就是undefined，done表示遍历是否结束
+
+//yield只能在Generator中实现，forEach中也不行都会报错 
+function* gen(){
+  // some code
+}
+
+var g = gen();
+
+g[Symbol.iterator]() === g //true
+//Generator 函数就是遍历器生成函数 和对象的Symbol.iterator属性相同
+
+Object.prototype[Symbol.iterator] = function*(){//给Object添加Iterator接口
+    let propKeys = Object.keys(this);
+    for (let propKey of propKeys) {
+        yield [propKey, this[propKey]];
+      }
+}
 ```
 ***
+#### 2.next方法
+***
+yield表达式本身没有返回值，或者说总是返回undefined。next方法可以带一个参数， _该参数就会被当作上一个yield表达式的返回值_。
+```javascript
+function* foo(x) {
+  var y = 2 * (yield (x + 1));
+  var z = yield (y / 3);
+  return (x + y + z);
+}
+
+var a = foo(5);
+a.next() // Object{value:6, done:false}
+a.next() // Object{value:NaN, done:false}
+a.next() // Object{value:NaN, done:true}
+
+var b = foo(5);
+b.next() // { value:6, done:false }
+b.next(12) // { value:8, done:false }
+b.next(13) // { value:42, done:true }
+```
+上面的代码很明确的给出带参数和不带参数的区别
+***
+#### 3. for...of 循环
+***
+for...of 循环可以自动遍历Generator函数运行时生成的Iterator对象，此时不需要调用next
+```javascript
+function* foo(){
+    yield 1;
+    yield 2;
+    yield 3;
+    yield 4;
+    yield 5;
+    return 6;//for...of不执行return
+}
+for(let v of foo()){
+    console.log(v);
+}
+console.log(foo().next());
+// 1
+// 2
+// 3
+// 4
+// 5
+// { value: 1, done: false }
+```
+***
+#### 4. Generator.prototype.throw()
+***
+```javascript
+var g = function* () {
+  try {
+    yield;
+  } catch (e) {
+    console.log('内部捕获', e);
+  }
+};
+
+var i = g();
+i.next();
+
+try {
+  i.throw('a');//注意可以有参数
+  i.throw('b');
+} catch (e) {
+  console.log('外部捕获', e);
+}
+// 内部捕获 a
+// 外部捕获 b
+```
++ 使用Generator对象的throw 如果Generator函数内部有try...catch，就会被内部捕获，如果没有就会被外部catch捕获，外部的throw只能被外部捕获  
++ throw方法抛出的错误要被内部捕获，前提是必须至少执行过一次next方法。因为没有执行next就表示Generator函数还没有执行
++ throw被内部捕获以后next会默认的执行一次
++ Generator函数内部的try...catch也是受执行顺序的营销，如果try...catch已经执行过在抛出错误，也是接受不到错误脚本掉，反之亦然
+***
+#### 5. Generator.prototype.return()
+***
++ Generator 函数返回的遍历器对象，还有一个return方法，可以返回给定的值，并且终结遍历 Generator 函数。如果没有给定值，会返回undefined done 为true
++ 如果 Generator 函数内部有try...finally代码块，且正在执行try代码块，那么return方法会导致立刻进入finally代码块，执行完以后，整个函数才会结束。
+***
+#### 6 next(),throw(),return()共同点
++ 共同点就是把对应eild表达式换成对应的参数
+#### 7 yield* 表达式
+***
++ yield* 用来调用用Generator函数
+```javascript
+function* foo1() {
+    yield 'a';
+    yield 'b';
+}
+
+function* bar1() {
+    yield 'x';
+    // 手动遍历 foo()
+    for (let i of foo1()) {
+        yield i;
+    }
+    yield 'y';
+}
+function* bar2(){
+    yield 'x';
+    yield* foo1();
+    yield 'y';
+}
+
+for (let v of bar1()){
+    console.log('bar1:'+v);
+}
+for (let v of bar2()){
+    console.log('bar2:'+v);
+}
+// bar1:x
+// bar1:a
+// bar1:b
+// bar1:y
+// bar2:x
+// bar2:a
+// bar2:b
+// bar2:y
+```
++ yield* 表达式是一个Generator函数，该函数返回的遍历器会自己执行遍历操作
++ yield* 表达式是一个对象时，如果该对象有Iterator接口，就会自己调用
+***
+#### 8. 作为对象属性的Genrator函数
+```javascript
+let obj = {
+  * myGeneratorMethod() {
+    ···
+  }
+};
+```
+#### 9. Generator函数的this
--- ES6/demo15/test_Generator.js
+++ ES6/demo15/test_Generator.js
@@ -5,8 +5,8 @@ function* helloWorldGenerator(){
 }
 var hw = helloWorldGenerator();
 // console.log(hw);
-// console.log(hw.next());
-// console.log(hw.next());
+// console.log(hw.next(1));
+// console.log(hw.next(1));
 // console.log(hw.next());
 // console.log(hw.next());
 // console.log(hw.next());
@@ -16,5 +16,187 @@ function* gen(){
     yield a = 123 + 456;
     return a;
 } 
-console.log(gen().next())
-console.log(gen().next())
+// console.log(gen().next())
+// console.log(gen().next())
+
+let arr = ['a', 'b', 'c'];
+let iter = arr[Symbol.iterator]();
+
+// console.log(iter.next()
+// ,iter.next()
+// ,iter.next()
+// ,iter.next())
+
+function* foo(){
+    yield 1;
+    yield 2;
+    yield 3;
+    yield 4;
+    yield 5;
+    return 6;
+}
+// for(let v of foo()){
+//     console.log(v);
+// }
+// console.log(foo().next());
+function* fibonacci() {
+    let [prev, curr] = [0, 1];
+    for (;;) {
+      yield curr;
+      [prev, curr] = [curr, prev + curr];
+    }
+  }
+  
+//   for (let n of fibonacci()) {
+//     if (n > 1000000000000) break;
+//     console.log(n);
+//   }
+Object.prototype[Symbol.iterator] = function*(){
+    let propKeys = Object.keys(this);
+    for (let propKey of propKeys) {
+        yield [propKey, this[propKey]];
+      }
+}
+// console.log(Object.prototype);
+
+var obj = {
+    a:1,
+    b:2,
+    c:3
+}
+// for(let i of obj){
+//     console.log(i);
+// }
+// for (let i in obj){
+//     console.log(i);
+// }
+// var g = function* () {
+//     try {
+//         yield;
+//     } catch (e) {
+//         console.log('内部捕获', e);
+//     }
+// };
+
+// var i = g();
+// i.next();
+
+// try {
+//     i.throw('a');
+//     i.throw('b');
+// } catch (e) {
+//     console.log('外部捕获', e);
+// }
+
+var gen1 = function* gen(){
+    // yield console.log('a');
+    try {
+        yield console.log('d');
+      } catch (e) {
+        // ...
+      }
+    yield console.log('b');
+    yield console.log('c');
+  }
+  
+//   var g1 = gen1();
+//   g1.next() // a
+// //   g1.next() // a
+//   g1.throw() // b
+//   g1.next() // c
+function* genretrun(){
+    console.log('next1')
+    yield console.log('1');
+    console.log('next2')
+    yield console.log('2');
+    console.log('next3')
+    yield console.log('3');
+    console.log('next4')
+    yield '2';
+    yield '3';
+    return console.log('5');
+}
+// var reg = genretrun();
+// reg.next();
+// reg.return('1');
+// console.log(reg.next());
+
+// console.log(genretrun().return(1))
+// function* foo(x) {
+//     var y = 2 * (yield (x + 1));
+//     y = 3;
+//     var z = yield (y / 3);
+//     return (x + y + z);
+//   }
+  
+//   var a = foo(5);
+//   console.log(a.next()) // Object{value:6, done:false}
+//   console.log(a.next()) // Object{value:NaN, done:false}
+//   console.log(a.next()) // Object{value:NaN, done:true}
+  
+//   var b = foo(5);
+//   console.log(b.next()) // { value:6, done:false }
+//   console.log(b.next(12)) // { value:8, done:false }
+//   console.log(b.next(13)) // { value:42, done:true }
+
+function* foo1() {
+    yield 'a';
+    yield 'b';
+}
+
+function* bar1() {
+    yield 'x';
+    // 手动遍历 foo()
+    for (let i of foo1()) {
+        yield i;
+    }
+    yield 'y';
+}
+function* bar2(){
+    yield 'x';
+    yield* foo1();
+    yield 'y';
+}
+
+// for (let v of bar1()){
+//     console.log('bar1:'+v);
+// }
+// for (let v of bar2()){
+//     console.log('bar2:'+v);
+// }
+// var isfor = true;
+// var bar1obj = bar1();
+// for (;isfor;){
+//     let mynext =  bar1obj.next();
+//     console.log(mynext);
+//     isfor=!mynext.done;
+// }
+
+function* foow() {
+    yield 2;
+    yield 3;
+    return "foo";
+  }
+  
+  function* barw() {
+    yield 1;
+    var v = yield* foow();
+    console.log("v: " + v);
+    yield 4;
+  }
+  
+  var it = barw();
+  
+  console.log(it.next())
+  console.log(it.next())
+  console.log(it.next())
+  console.log(it.next())
+  // {value: 1, done: false}
+//   it.next()
+//   // {value: 2, done: false}
+//   it.next()
+//   // {value: 3, done: false}
+//   it.next();
+//   // "v: foo"
+//   // {value: 4, done: false}
+//   it.next()
